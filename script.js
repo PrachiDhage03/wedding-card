d8f7eaa664a5b31989c579d968db24384a338157
Prachi Dhage <prachid1406@gmail.com>
2026-04-24 11:40:42 +0530

    final commit

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
     guestElement.textContent = formattedName;
     console.log("Guest name set:", formattedName);
   } else {
-    guestElement.textContent = " ";
+    guestElement.textContent = " संजय तुकाराम धागे";
     console.log("No guest name - using default");
   }
 });
